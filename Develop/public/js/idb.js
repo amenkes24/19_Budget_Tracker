c8fa4b7e6336d4212c53b16c6f435804e32057cb
Adam Menkes <amenkes24@gmail.com>
2022-02-16 11:51:20 -0800

    create idb file

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -14,10 +14,9 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
 
-      // check if app is online, if yes run uploadPizza() function to send all local db data to api
+      // check if app is online, if yes run sendTransaction() function to send all local db data to api
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
-    // uploadPizza();
+    sendTransaction();
   }
 };
 
@@ -26,9 +25,65 @@ request.onerror = function(event) {
   console.log(event.target.errorCode);
 };
 
+// this function will be executed if we attempt to submit new budget data and there's no internet 
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions 
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+
+  // access the object store for `new_budget`
+  const budgetObjectStore = transaction.objectStore('new_budget');
+
+  // add record to your store with add method
+  budgetObjectStore.add(record);
+}
 
 
+function sendTransaction()  {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_budget'], 'readwrite');
 
+  // access your object store
+  const budgetObjectStore = transaction.objectStore('new_budget');
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          // access the new_budget object store
+          const budgetObjectStore = transaction.objectStore('new_budget');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All saved data has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+
+// listen for app coming back online
+window.addEventListener('online', sendTransaction);
 
 
 
